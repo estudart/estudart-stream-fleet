e1837cd3390613b4ee97d6560783db5465111270
Studart Erico <erico.studart@bcg.com>
2026-04-18 18:43:25 -0300

    fix: set variable as env var

--- client/src/streamfleet_client/main_consumer.py
+++ client/src/streamfleet_client/main_consumer.py
@@ -1,10 +1,12 @@
 import asyncio
+import os
 
 from src.streamfleet_client.presentation.dependencies import get_websocket_consumer
 
+CHANNEL = os.environ.get("CHANNEL")
 
 async def main():
-    wss = get_websocket_consumer("4856177744")
+    wss = get_websocket_consumer(CHANNEL)
     async for payload in wss.get_messages():
         preview = payload[:72] + "..." if len(payload) > 72 else payload
         print(f"{len(payload)} chars: {preview}")
